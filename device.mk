ad20eb3d9b1402886970351b7cd945817e993e2e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:56:54 +0200

    vayu: Build custom QTI vibrator AIDL
    Change-Id: Ica7bd62e1328bd2d3a0278c6ee3901bddb2cb95d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -478,7 +478,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_sm8150
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
