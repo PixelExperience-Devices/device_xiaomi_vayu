129f7959f183f63b72ef5d1873ede31f03091b50
kubersharma001 <kubersharma001@gmail.com>
2021-10-29 16:20:53 +0000

    vayu: [TEMPORARY] Allow neverallows
    Change-Id: I149c83e2e8dae31c88c1ae29e044851fd94c6f6e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,6 +160,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Vendor Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
