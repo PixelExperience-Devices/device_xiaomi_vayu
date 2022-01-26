8b31c2f4781b3675af5c65a3e769afe5d464488d
Rafiester <rafi.haqqi@gmail.com>
2022-01-26 11:07:57 +0700

    vayu: Switch to QTI Bluetooth stack
    * Also kang needed ANT blobs from AOSPA
    
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- device.mk
+++ device.mk
@@ -81,15 +81,14 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
+    libbthost_if \
+    libtinycompress \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
@@ -429,7 +428,9 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # NeuralNetworks
 PRODUCT_PACKAGES += \
