2fd603ad6307f7d3bb99edd03c7890c80a2c3c02
Alisson Grizotti <alisson1397@hotmail.com>
2022-02-21 23:52:39 -0300

    vayu: Use AOSP Recovery

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -15,6 +15,7 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
