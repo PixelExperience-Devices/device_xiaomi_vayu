c7d1ea95a288410fe915025aee0e5805099ad78d
AlissonGrizotti <alisson1397@hotmail.com>
2022-08-19 20:48:47 +0530

    Revert "vayu: Build custom QTI vibrator AIDL"
    This reverts commit d90d3b894bf70672672dc46094ad559b69e15b6e.

--- device.mk
+++ device.mk
@@ -483,7 +483,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_sm8150
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
