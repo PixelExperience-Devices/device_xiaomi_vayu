f9e4a64191cf8837f5971a33f9246fe138d776ba
Bagualisson <alisson1397@hotmail.com>
2023-01-01 16:47:03 -0300

    vayu: sepolicy: Switch to sepolicy_vndr-legacy-um

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,7 +161,7 @@ TARGET_USES_MKE2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
